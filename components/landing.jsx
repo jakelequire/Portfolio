b34742e311232767f09e730b7a3b01ae60cdf935
Phock <jakelequire@gmail.com>
2022-12-21 22:54:30 -0800

    Navbar Part2

--- components/landing.jsx
+++ components/landing.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Landing() {
   return (
     <div className="landing-wrapper">
-        <div className="hero-container">
-          
+        <div className="hero-container" id="l-home">
+
         </div>
     </div>
   );
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,41 +1,50 @@
 import { useRef, useEffect } from "react";
 
-function NavBar() {
+export default function NavBar() {
+  // Create a ref to store the nav DOM element
   const navRef = useRef(null);
+  
   useEffect(() => {
     // Add event listeners
     window.addEventListener("scroll", handleScroll);
-    // Remove event listeners on cleanup
+
     return () => {
+      // Remove event listeners on cleanup
       window.removeEventListener("scroll", handleScroll);
     };
   }, []); // Empty array means this effect only runs once when the component mounts
 
   const handleScroll = () => {
+    // Get nav element and its height
     const nav = navRef.current;
     const navHeight = nav.offsetHeight;
     const scrollHeight = window.pageYOffset;
+
+    // If scroll height is greater than nav height, add class to change nav style
     if (scrollHeight > navHeight) {
       nav.classList.add("nav-scroll");
       nav.style.background = "rgba(206, 206, 206, 0.20)"
       nav.querySelectorAll('.nav-link').forEach(link => {
         link.style.color = '#fff';
       });
-    } else {
+
+    // If scroll height is less than nav height, remove class to change nav style
+    } else { 
       nav.classList.remove("nav-scroll");
       nav.style.background = "#ffffff"
       nav.querySelectorAll('.nav-link').forEach(link => {
-        link.style.color = '';
+        link.style.color = '#000';
       });
     }
   }
 
+  // Render the navbar
   return (
       <div>
         <div className="navbar" ref={navRef}>
           <div className="nav-group">
             <div className="nav-links-container">
-              <a className="nav-link nav-item " id="home" href="/">Home</a>
+              <a className="nav-link nav-item " id="home" href="#l-home">Home</a>
                 <span className="nav-separator">/</span>
               <a className="nav-link nav-item " id="about" href="/">About Me</a>
                 <span className="nav-separator">/</span>
@@ -48,5 +57,3 @@ function NavBar() {
       </div>
     );
 }
-
-export default NavBar;
