9989531ed02a31244080947b55372a83e7841549
Phock <jakelequire@gmail.com>
2023-01-02 18:12:11 -0800

    Contact section changes

--- components/contact.jsx
+++ components/contact.jsx
@@ -8,32 +8,39 @@ export default function Contact() {
             <div className="contact-header">
                 <h1 className="contact-title"><span id="contact-C">C</span>ontact Me!</h1>
             </div>
-            <div className="contact">
-                <form className="contact-form" action="/contact" method="POST">
-                    <div className="form-top">
-                        <div className="form-name">
-                            <label id="f-name" htmlFor="name">Name</label>
-                            <input type="text" name="name" id="name" />
+            <div className="contact-info">
+                <div className="contact">
+                    <form className="contact-form" action="/contact" method="POST">
+                        <div className="form-top">
+                            <div className="form-name">
+                                <label id="f-name" htmlFor="name">Name</label>
+                                <input type="text" name="name" id="name" />
+                            </div>
+                            <div className="form-email">
+                                <label id="f-email" htmlFor="email">Email</label>
+                                <input type="email" name="email" id="email" />
+                            </div>
                         </div>
-                        <div className="form-email">
-                            <label id="f-email" htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" />
+                        <div className="form-body">
+                            <div className="form-subject">
+                                <label id="f-subject" htmlFor="subject">Subject</label>
+                                <input type="text" name="subject" id="subject" />
+                            </div>
+                            <div className="form-message">
+                                <label id="f-message" htmlFor="message">Message</label>
+                                <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                            </div>
                         </div>
+                        <div className="form-button">
+                            <button href={null} className="f-btn" type="submit">Send</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="resume-container">
+                    <div className="resume">
+        
                     </div>
-                    <div className="form-body">
-                        <div className="form-subject">
-                            <label id="f-subject" htmlFor="subject">Subject</label>
-                            <input type="text" name="subject" id="subject" />
-                        </div>
-                        <div className="form-message">
-                            <label id="f-message" htmlFor="message">Message</label>
-                            <textarea name="message" id="message" cols="30" rows="10"></textarea>
-                        </div>
-                    </div>
-                    <div className="form-button">
-                        <button href={null} className="f-btn" type="submit">Send</button>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
     )
