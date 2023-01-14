4ddc8e9200e87c91acff21ee3f25afe1e497c9df
Phock <jakelequire@gmail.com>
2023-01-14 00:24:49 -0800

    ###

--- components/blog/blog-landing.jsx
+++ components/blog/blog-landing.jsx
@@ -1,13 +1,44 @@
 import { useState, useEffect, useRef, useCallback } from 'react'
+import lottie from 'lottie-web'
 import useCustomHover from '../hooks/useCustomHover.jsx'
 import useCustomFilter from '../hooks/useCustomFilter.jsx'
+
+import arrowAnimation from '../../public/media/animated/arrow.json'
 
 export default function BlogLanding() {
     const [tagHover, tagRef] = useCustomHover()
     const [categoryHover, categoryRef] = useCustomHover()
-    
-    
+
     const { filter, setFilter, setCategory, setTag } = useCustomFilter();
+
+    /* Dropdown animation  */
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+    const animationRef = useRef(null)
+
+    useEffect(() => {
+        animationRef.current = lottie.loadAnimation({
+          container: animationRef.current,
+          renderer: 'svg',
+          loop: false,
+          autoplay: false,
+          animationData: arrowAnimation
+        })
+      }, [])
+    
+      useEffect(() => {
+        if (categoryHover) {
+          animationRef.current.playSegments([0, 10], true);
+          animationRef.current.setDirection(1);
+        } else {
+            animationRef.current.playSegments([0, 0], true);
+          animationRef.current.setDirection(-1);
+        }
+      }, [categoryHover]);
+      
+    
+      
+
+    
 // >------------------------------------------------------------------------------------------
     return (
         <div className="blog-landing">
@@ -29,10 +60,11 @@ export default function BlogLanding() {
                             All</a>
                             
                             <div ref={categoryRef} className="filter-dropdown-category">
-                            <a className="filter-header"
+                            <a className="filter-header" ref={animationRef}
+                            onMouseEnter={() => setIsDropdownOpen(true)} onMouseLeave={() => setIsDropdownOpen(false)}
                             id={categoryHover ? 'filters-active' : 'filters-inactive'}>Category</a>     
                                 {categoryHover ? (
-                                    <div className='A-filter-dropdown-category'
+                                    <div  className='A-filter-dropdown-category'
                                     id={categoryHover ? 'category-active' : 'category-inactive'}>
                                       <a className="filter-dropdown-item"
                                       onClick={() => setCategory(prev => !prev)}>Category 1</a>
@@ -45,7 +77,8 @@ export default function BlogLanding() {
                             </div> {/* Note to self: add a button that will reset the filters */}
 
                             <div ref={tagRef} className="filter-dropdown-tags">
-                                <a className="filter-header"
+                                <a className="filter-header" ref={animationRef}
+                                onMouseEnter={() => setIsDropdownOpen(true)} onMouseLeave={() => setIsDropdownOpen(false)}
                                 id={tagHover ? 'filters-active' : 'filters-inactive'}>Tags</a>
                                 {tagHover ? (
                                     <div className='A-filter-dropdown-tags'
