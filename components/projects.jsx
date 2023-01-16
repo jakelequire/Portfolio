432eda50d8d52bbb71bc0f8515cf4a45bdd61d68
Phock <jakelequire@gmail.com>
2023-01-15 18:01:02 -0800

    ###

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
-
+/* -- Hooks -- */
 import useCustomSmoothScroll from "./hooks/useCustomSmoothScroll.jsx";
 /* -- SVG -- */
 import nextjs from "../public/media/icons/tech-branding/next3.svg";
@@ -12,7 +12,7 @@ import nodejs from "../public/media/icons/tech-branding/nodejs.svg";
 import SolBobaHomepage from "../public/media/pictures/SolBobaHomepage.png";
 import PortfolioPreview from "../public/media/pictures/portfolio-preview.png";
 import PortfolioBackendPreview from "../public/media/pictures/PortfolioBackend-preview.png";
-
+// >------------------------------------------------------------------------------------------
 export default function Projects() {
   const { refs } = useCustomSmoothScroll();
 
@@ -23,28 +23,36 @@ export default function Projects() {
       description:
         "Small business website for a local boba tea shop. My first project!",
       technologies: [nextjs, react, javascript],
+      id: "project1",
     },
     project2: {
       name: "Portfolio",
       description:
         "My personal portfolio website. Built to learn backend and frontend development.",
       technologies: [nextjs, react, javascript],
+      id: "project2",
     },
     project3: {
       name: "Portfolio - Backend",
       description:
         "Backend for my personal portfolio website. Built to learn backend development.",
       technologies: [nextjs, nodejs, javascript],
+      id: "project3",
     },
     project4: {
       name: "Coming Soon",
       description: "New project coming soon! Practice for learning TypeScript.",
       technologies: [nextjs, react, typescript],
+      id: "project4",
     },
   };
   /* Project layout template */
   const projectItems = Object.values(projects).map((project, index) => (
-    <div className="project-item" key={index} onClick={() => setSelectedProject(project)}>
+    <div
+      className="project-item"
+      key={index}
+      onClick={() => setSelectedProject(project)}
+    >
       <div className="project-item-wrapper">
         <h2 className="project-item-name">{project.name}</h2>
         <p className="project-item-description">{project.description}</p>
@@ -63,51 +71,71 @@ export default function Projects() {
     </div>
   ));
   const [selectedProject, setSelectedProject] = useState(projectItems[0]);
+  /* ----------------- */
+
+  /* Project image data */
+  const projectImages = {
+    project1: {
+      image: SolBobaHomepage,
+      alt: "Sol Boba Homepage",
+      github: "https://github.com/jakelequire/SolBoba",
+      website: "https://solboba.com/",
+    },
+    project2: {
+      image: PortfolioPreview,
+      alt: "Personal Portfolio",
+      github: "https://github.com/jakelequire/Portfolio",
+      website: "https://jakelequire.dev/",
+    },
+    project3: {
+      image: PortfolioBackendPreview,
+      alt: "Personal Portfolio - Backend",
+      github: "https://github.com/jakelequire/portfolio-backend",
+      website: "https://jakelequire.dev/",
+    },
+    project4: {
+      image: SolBobaHomepage,
+      alt: "Coming Soon",
+      github: "",
+      website: "placeholder",
+    },
+  };
+  /* Image layout template */
+  const projectImage = Object.values(projectImages).map((project, index) => (
+    <div className="project-preview-image" key={index}>
+      <Image
+        src={project.image}
+        className="preview-image"
+        alt={project.alt}
+        width={774}
+        height={640}
+      />
+      <div className="project-link-container">
+        <a
+          href={project.website}
+          target="_blank"
+          rel="noreferrer"
+          className="project-link visit-site"
+        >
+          Visit Site
+        </a>
+        <a
+          href={project.github}
+          target="_blank"
+          rel="noreferrer"
+          className="project-link view-code"
+        >
+          View Code
+        </a>
+      </div>
+    </div>
+  ));
+  /* ----------------- */
+
+
+
+
 	
-	/* ----------------- */
-	const projectImages = {
-		project1: {
-			image: SolBobaHomepage,
-			alt: "Sol Boba Homepage",
-			github: "https://github.com/jakelequire/SolBoba",
-			website: "https://solboba.com/",
-		},
-		project2: {
-			image: PortfolioPreview,
-			alt: "Personal Portfolio",
-			github: "https://github.com/jakelequire/Portfolio",
-			website: "https://jakelequire.dev/",
-		},
-		project3: {
-			image: PortfolioBackendPreview,
-			alt: "Personal Portfolio - Backend",
-			github: "https://github.com/jakelequire/portfolio-backend",
-			website: "https://jakelequire.dev/",
-		},
-		project4: {
-			image: SolBobaHomepage,
-			alt: "Coming Soon",
-			github: "",
-			website: "placeholder",
-		},
-	}
-
-	const projectImage = Object.values(projectImages).map((project, index) => (
-		<div className="project-preview-image" key={index}>
-			<Image src={project.image} className="preview-image" alt={project.alt} width={774} height={640} />
-			<div className="project-link-container">
-				<a href={project.website} 
-				target="_blank" 
-				rel="noreferrer" 
-				className="project-link visit-site">Visit Site</a>
-				<a href={project.github}
-				target="_blank"
-				rel="noreferrer"
-				className="project-link view-code">View Code</a>
-			</div>
-		</div>
-	))
-
   // >------------------------------------------------------------------------------------------
   return (
     <div className="projects-wrapper" id="projects" ref={refs[2]}>
@@ -119,16 +147,22 @@ export default function Projects() {
       <div className="projects-container">
         <div className="project-list">
           <div className="project-item-container">
-            {projectItems[0]}
-            {projectItems[1]}
-            {projectItems[2]}
-            {projectItems[3]}
+            <a className="project-items" id={projects.project1.id}>
+              {projectItems[0]}
+            </a>
+            <a className="project-items" id={projects.project2.id}>
+              {projectItems[1]}
+            </a>
+            <a className="project-items" id={projects.project3.id}>
+              {projectItems[2]}
+            </a>
+            <a className="project-items" id={projects.project4.id}>
+              {projectItems[3]}
+            </a>
           </div>
         </div>
 
-        <div className="project-preview">
-					{projectImage[0]}
-        </div>
+        <div className="project-preview">{projectImage[0]}</div>
       </div>
     </div>
   );
