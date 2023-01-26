d3d994b2ab6f5c382f2173d70980f1424978de6a
Phock <jakelequire@gmail.com>
2023-01-26 07:12:22 -0800

    ###

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { ResponsiveRadar } from '@nivo/radar'
+import data from '../public/data/chartData.json'
 /* -- Hooks -- */
 import useObserver from "./hooks/useCustomSmoothScroll.jsx";
 
@@ -7,7 +8,7 @@ import useObserver from "./hooks/useCustomSmoothScroll.jsx";
 export default function Projects() {
   const { ref } = useObserver();
 
-
+ 
 
   // >------------------------------------------------------------------------------------------
   return (
@@ -32,7 +33,42 @@ export default function Projects() {
               <div className="data-wrapper">
                 <div className="preview-sidebar"></div>
                 <div className="project-data">
-
+                <ResponsiveRadar
+                  data={data}
+                  keys={[ 'chardonay', 'carmenere', 'syrah' ]}
+                  indexBy="language"
+                  valueFormat=">-.2f"
+                  margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+                  borderColor={{ from: 'color' }}
+                  gridLabelOffset={36}
+                  dotSize={10}
+                  dotColor={{ theme: 'background' }}
+                  dotBorderWidth={2}
+                  colors={{ scheme: 'nivo' }}
+                  blendMode="multiply"
+                  motionConfig="wobbly"
+                  legends={[
+                      {
+                          anchor: 'top-left',
+                          direction: 'column',
+                          translateX: -50,
+                          translateY: -40,
+                          itemWidth: 80,
+                          itemHeight: 20,
+                          itemTextColor: '#999',
+                          symbolSize: 12,
+                          symbolShape: 'circle',
+                          effects: [
+                              {
+                                  on: 'hover',
+                                  style: {
+                                      itemTextColor: '#000'
+                                  }
+                              }
+                          ]
+                      }
+                  ]}
+                  />
                 </div>
               </div>
             </div>
