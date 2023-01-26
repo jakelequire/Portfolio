0fd4c1728d0e21d93a8dd84c407af91dd83c90d5
Phock <jakelequire@gmail.com>
2023-01-26 09:25:57 -0800

    ###

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,21 +1,36 @@
 import { useState, useEffect, useRef } from "react";
+import Image from "next/image.js";
 import { ResponsiveRadar } from '@nivo/radar'
-import data from '../public/data/chartData.json'
+/* -- Data -- */
+import data from '../public/data/radarChart_One.json'
+// import dataTwo from '../public/data/treeMap_One.json'
 /* -- Hooks -- */
 import useObserver from "./hooks/useCustomSmoothScroll.jsx";
-
+/* SVGs */
+import RadarChart from '../public/media/icons/web-media/radar-chart.svg'
+import TreeMap from '../public/media/icons/web-media/treemap-chart.svg'
 // >------------------------------------------------------------------------------------------
 export default function Projects() {
   const { ref } = useObserver();
 
- 
+ const [iconHover, setIconHover] = useState(false);
+
+  useEffect(() => {
+    const icon = document.querySelector('.icon-radar-chart');
+    icon.addEventListener('mouseover', () => {
+      setIconHover(true);
+    });
+    icon.addEventListener('mouseout', () => {
+      setIconHover(false);
+    });
+  }, []);
 
   // >------------------------------------------------------------------------------------------
   return (
     <div className="projects-wrapper" id="projects" ref={ref.projects}>
       <div className="projects-container">
         <div className="project-list">
-
+          {/* -- List of Projects -- */}
           <div className="project-heading">
             <div className="project-item">
               <div className="project-title">
@@ -25,33 +40,51 @@ export default function Projects() {
 
               </div>
             </div>
-
+            {/* -- Data -- */}
             <div className="project-description">
               <div className="preview-header">
-                <h1 className="preview-title">Data.</h1>
+                <h1 className="preview-title">Project Data.</h1>
               </div>
               <div className="data-wrapper">
-                <div className="preview-sidebar"></div>
-                <div className="project-data">
-                <ResponsiveRadar
-                  data={data}
-                  keys={[ 'chardonay', 'carmenere', 'syrah' ]}
-                  indexBy="language"
-                  valueFormat=">-.2f"
-                  margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
-                  borderColor={{ from: 'color' }}
-                  gridLabelOffset={36}
-                  dotSize={10}
-                  dotColor={{ theme: 'background' }}
-                  dotBorderWidth={2}
-                  colors={{ scheme: 'nivo' }}
-                  blendMode="multiply"
-                  motionConfig="wobbly"
-                  legends={[
-                      {
-                          anchor: 'top-left',
+                <div className="preview-sidebar">
+                  <a className="a-data-link"
+                  id={iconHover ? "data-active" : "data-inactive"}>
+                    <Image className="icon-radar-chart"
+                    src={RadarChart}
+                    alt="Radar Chart"
+                    title="Radar Chart"
+                    width={50}
+                    height={50} />
+                  </a>
+                  <a className="a-data-link">
+                    <Image className="icon-tree-map"
+                    src={TreeMap}
+                    alt="Tree Map"
+                    title="Tree Map"
+                    width={50}
+                    height={50} />
+                  </a>
+                </div>
+                  <div className="project-data">
+                    <ResponsiveRadar
+                      data={data}
+                      keys={[ 'lines', 'files', 'percentage' ]}
+                      indexBy="language"
+                      valueFormat=">-.2f"
+                      margin={{ top: 70, right: 80, bottom: 40, left: 40 }}
+                      borderColor={{ from: 'color' }}
+                      gridLabelOffset={25}
+                      dotSize={5}
+                      dotColor={{ theme: 'background' }}
+                      dotBorderWidth={5}
+                      blendMode="multiply"
+                      motionConfig="wobbly"
+                      colors={{ scheme: 'dark2' }}
+                      legends={[
+                        {
+                          anchor: 'top-right',
                           direction: 'column',
-                          translateX: -50,
+                          translateX: -170,
                           translateY: -40,
                           itemWidth: 80,
                           itemHeight: 20,
@@ -59,25 +92,25 @@ export default function Projects() {
                           symbolSize: 12,
                           symbolShape: 'circle',
                           effects: [
-                              {
-                                  on: 'hover',
-                                  style: {
-                                      itemTextColor: '#000'
-                                  }
+                            {
+                              on: 'hover',
+                              style: {
+                                  itemTextColor: '#000'
                               }
+                            }
                           ]
-                      }
-                  ]}
-                  />
-                </div>
+                        }
+                      ]}
+                    />
+                  </div>
               </div>
             </div>
           </div>
-
+          {/* -- Project Preview -- */}          
           <div className="project-preview">
             <div className="preview-wrapper">
 
-              </div>
+            </div>
           </div> 
 
         </div>
