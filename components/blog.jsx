e6a69973a5792430ecfe68b6ed05cafb529dc5ab
Phock <jakelequire@gmail.com>
2023-01-29 13:17:25 -0800

    ###

--- components/blog.jsx
+++ components/blog.jsx
@@ -2,11 +2,30 @@ import useObserver  from "./hooks/useCustomSmoothScroll.jsx";
 
 export default function Blog() {
     const { ref } = useObserver();
-
+    
 // >------------------------------------------------------------------------------------------
     return (
         <div className="blog-wrapper" id="blog" ref={ref.blog}>
-            
+            <div className="blog-container">
+                <div className="blog-featured-wrapper">
+                    <div className="blog-featured">
+                        <h1 className="blog-featured-title">Featured Article.</h1>
+                        <div className="blog-featured-content">
+
+                        </div>
+                    </div>
+                    <div className="blog-recents">
+
+
+                    </div>
+                </div>
+                <div className="blog-preview-wrapper">
+                    <div className="blog-preview">
+
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
