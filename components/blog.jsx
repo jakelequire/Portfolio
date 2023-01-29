cfba90968a2f09e9f9e6e6dfec5916a1f4f8040d
Phock <jakelequire@gmail.com>
2023-01-29 13:30:10 -0800

    ###

--- components/blog.jsx
+++ components/blog.jsx
@@ -7,21 +7,32 @@ export default function Blog() {
     return (
         <div className="blog-wrapper" id="blog" ref={ref.blog}>
             <div className="blog-container">
+
                 <div className="blog-featured-wrapper">
+                    
                     <div className="blog-featured">
-                        <h1 className="blog-featured-title">Featured Article.</h1>
+                        <div className="featured-header">
+                            <h2 className="blog-featured-title">Featured Article.</h2>
+                        </div>
                         <div className="blog-featured-content">
 
                         </div>
                     </div>
-                    <div className="blog-recents">
 
+                    <div className="blog-recents">
+                        <div className="recents-header">
+                            <h2 className="blog-preview-title">Recent Articles.</h2>
+                        </div>
 
                     </div>
+
                 </div>
+
                 <div className="blog-preview-wrapper">
                     <div className="blog-preview">
-
+                        <div className="preview-header">
+                            
+                        </div>
 
                     </div>
                 </div>
