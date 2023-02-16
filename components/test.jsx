de807c16f86b9a61a090043f9b6d1a87a5789536
jakelequire <114044604+jakelequire@users.noreply.github.com>
2023-02-16 14:31:54 -0800

    ###

--- components/test.jsx
+++ components/test.jsx
@@ -1,34 +1,15 @@
-import React, { useState } from 'react';
-import fetchArticles from '../pages/api/_fetchArticles.js';
+import { useState } from 'react';
+import exportArticles from '../pages/api/_fetchArticles.js';
+
+// >---------------------------------------------------------
 export default function Test_Dev() {
-  const [title, setTitle] = useState('');
-  const [categories, setCategories] = useState([]);
-  const [tags, setTags] = useState([]);
-  const [date, setDate] = useState('');
-  const [author, setAuthor] = useState('');
-  const [image, setImage] = useState('');
-  const [content, setContent] = useState('');
-
-  fetchArticles().then((data) => {
-    setTitle(data.title);
-    setCategories(data.categories);
-    setTags(data.tags);
-    setDate(data.date);
-    setAuthor(data.author);
-    setImage(data.image);
-    setContent(data.content);
-  });
 
 
+
+// >---------------------------------------------------------
   return (
     <div>
-      <h1>{title}</h1>
-      <h2>{categories}</h2>
-      <h3>{tags}</h3>
-      <h4>{date}</h4>
-      <h5>{author}</h5>
-      <h6>{image}</h6>
-      <p>{content}</p>
+
     </div>
   );
 };
--- pages/api/_fetchArticles.js
+++ pages/api/_fetchArticles.js
@@ -1,32 +1,49 @@
 import axios from "axios";
-
+/* 
+  ---
+  title: "Example ONE"
+  date: "1995-10-6"
+  content: "Content??"
+  tags: ["React", "Next"]
+  category: "Web Development"
+  image: "meme.jpg"
+  imageAlt: "Meme"
+  ---
+*/
 class Article {
-    constructor(title, description, tags, date, author, content, image, imageAlt) {
+    constructor(title, date, content, tags, category, image, imageAlt) {
         this.title = title;
-        this.description = description;
-        this.tags = tags;
         this.date = date;
-        this.author = author;
         this.content = content;
+        this.tags = tags;
+        this.category = category;
         this.image = image;
         this.imageAlt = imageAlt;
     }
 }
+/**
+ * The main function that exports the formatted articles.
+ * 
+ * @returns {Promise} A promise that resolves to an array of `Article` objects.
+ */
+export default async function exportArticles() {
 
-export default async function fetchArticles() {
-    const { data } = await axios.get("http://localhost:3001/articles");
-    const articles = await data.map((article) => {
-        return new Article(
-            article.title, 
-            article.description, 
-            article.tags, 
-            article.date, 
-            article.author, 
-            article.content, 
-            article.image, 
-            article.imageAlt
-        );
+}
+/**
+ * Sends a `GET` request to the server to get all articles.
+ * 
+ * @returns {Promise} A promise that resolves to an array of articles.
+ */
+async function importArticles() {
+    axios.get("http://localhost:3001/articles").then((res) => {
+        console.log(res.data);
     });
-    console.log(articles)
-    return articles;
+}
+/**
+ * Takes the imported articles and formats into an array of `Article` objects.
+ * 
+ * @returns {Promise} A promise that resolves to an array of `Article` objects.
+ */
+async function formatArticles() {
+
 }
