f1060dab22dc0920eed76c891839656065f6fd52
Phock <jakelequire@gmail.com>
2023-02-19 11:34:23 -0800

    Update articleMarkdown.jsx

--- components/subComponents/articleMarkdown.jsx
+++ components/subComponents/articleMarkdown.jsx
@@ -1,1 +1,17 @@
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import importArticles from 'components/subComponents/importArticles.jsx';
+
+const Articles = async () => {
+    const articles = await importArticles();
+    const articleData = articles.map((article) => ({
+        title: article.title,
+        date: article.date,
+        content: article.content,
+        tags: article.tags,
+        category: article.category,
+        image: article.image,
+        imageAlt: article.imageAlt
+    }));
+    return articleData;
+}
+
